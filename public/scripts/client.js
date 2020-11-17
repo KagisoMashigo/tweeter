9932964cc73260ea473e5600efe68f652bf070a9
Iso <kagiso.k.mashigo@gmial.com>
2020-11-17 21:29:05 +0000

    client logic updated to include loadtweets, fetchtweets fcts and ajax requests. All working 100%

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,30 +1,5 @@
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
-
 const renderTweets = function(tweets) {
-// $('.tweet-body').empty();
+$('.tweet-body').empty();
   for (const tweet of tweets) {
    const value = createTweetElement(tweet);
    $('.tweet-body').prepend(value);
@@ -62,23 +37,27 @@ const postTweets = () => {
       url: "/tweets",
       method: "POST",
       data: $(this).serialize()
+    }).then(() => {
+      loadTweets();
     }).catch(err => console.log(err));
     console.log("success")
   });
 };
 
 // Loads tweets suing AJAX
-// const loadTweets = () => {
-//   console.log("Tweet fetched");
-//   $.ajax({
-//     url: "/tweets",
-//     method: "GET"
-//   }).then((data) => {
-//     renderTweets(data);
-//   }).catch(err => console.log(err));
-// };
+const loadTweets = () => {
+  console.log("Stock tweets fetched");
+  $.ajax({
+    url: "/tweets",
+    method: "GET",
+    data: "data"
+  }).then((data) => {
+    renderTweets(data);
+  }).catch(err => console.log(err));
+};
 
 $(document).ready(() => {
-  renderTweets(data);
   postTweets()
+  loadTweets()
 });
+console.log("Stock tweets fetched");

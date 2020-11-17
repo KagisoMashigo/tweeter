9a53aadd78ec318f5e7fb80817687b53bd03d3ad
Iso <kagiso.k.mashigo@gmial.com>
2020-11-17 23:14:36 +0000

    character counter updated

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,5 +1,5 @@
-$( "textarea" ).on("keyup", function() {
-  const wordCount =  $(this).parent().children().find(".counter");
+$("textarea").on("keyup", function() {
+  const wordCount =  $(this).parent().find(".counter");
   wordCount.text((140 - $(this).val().length));
-  wordCount.val() <= 0 ? wordCount.css({'color':'#e40a0a'}): wordCount.css({'color':'#545149'}); // its more advisable to code css into a seperate file
+  wordCount.val() <= 0 ? wordCount.css({'color':'#e40a0a'}) : wordCount.css({'color':'#545149'});
 });

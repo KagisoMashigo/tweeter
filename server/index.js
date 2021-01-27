e9af322fd6e362bc5cfcaac56656f826a44267c4
Masterkakes <kagiso.k.mashigo@gmail.com>
2021-01-27 16:31:01 -0500

    add PORT config

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 "use strict";
 // Basic express setup:
 
-const PORT          = 8080;
+const PORT          = process.env.PORT || 5000;
 const express       = require("express");
 const bodyParser    = require("body-parser");
 const app           = express();
